61c04a76e30ad6b91f5f65cd5890491bff2d1ea7
Noah <noahshomette@gmail.com>
2022-11-19 02:28:08 -0500

    Implemented basic trait based tile terrain system

---
+++ examples/ggfbasic.rs
@@ -0,0 +1,63 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+use bevy_ggf::camera::movement;
+use bevy_ggf::mapping::Map;
+use bevy_ggf::mapping::tiles::{Grassland, Hill, Ocean, TerrainExtensionTrait, TerrainExtensionTraitBase};
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let tilemap_size = TilemapSize { x: 100, y: 100 };
+    let tilemap_tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+
+    let tilemap_type = TilemapType::Square;
+    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
+
+    let map_texture_vec: Vec<Box<dyn TerrainExtensionTraitBase>> = vec![Box::new(Grassland{}), Box::new(Hill{}), Box::new(Ocean{})];
+    
+    let map = Map::generate_random_map(
+        &mut commands,
+        &tilemap_size,
+        &tilemap_type,
+        &tilemap_tile_size,
+        texture_handle, 
+        &map_texture_vec
+    );
+
+    commands.spawn(map);
+}
+
+fn despawn_map(
+    mut query: Query<(Entity, &Map)>,
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+) {
+    let (entity, map) = query.single_mut();
+
+    if keyboard_input.just_pressed(KeyCode::A) {
+        commands.entity(entity).despawn_descendants();
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin{
+            window: WindowDescriptor {
+                width: 1270.0,
+                height: 720.0,
+                title: String::from(
+                    "Basic Example - Press Space to change Texture and H to show/hide tilemap.",
+                ),
+                ..Default::default()
+            },
+            ..default()
+        }).set(ImagePlugin::default_nearest()))
+        .add_plugin(TilemapPlugin)
+        .add_system(movement)
+        .add_startup_system(startup)
+        .add_system(despawn_map)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .run();
+}
---
+++ src/mapping/tiles.rs
@@ -0,0 +1,176 @@
+// two types of terrain. TerrainBaseType and TerrainExtensionType - TerrainBaseType is a type like Ground, Water,
+// whatever else. TerrainExtensionType is like grass, mountain, forest, ocean, etc.
+// The two types work on a derivitive basis. The extension types are derivitives of a base type.
+
+// Terrains should work like this - you define the base type, and then you define the extension
+// types of that base type. The extension types are only definable or assignable from the base type
+// Maybe the defined base type holds a vec of the extension types or something.
+// You can't assign a base type to anything as its more of a category really rather than
+// a specific terrain. But you should be able to access the base type from an extension type
+
+// information we need to hold in terrain
+// Terrain type (both base and extension
+//       - but the extension should be derived from the base type in such a way that we are really
+//         only dealing with the extensions but we can access the base type from the extension as needed),
+// movement cost for each movement type - track, wheeled, etc (this would need to be extensible as well),
+//
+//
+//
+//
+
+// What does a tile need to hold?
+
+// Terrain information - movement related stuff and all that like movement costs
+// units in tile information - maybe just the entity?
+// the sprite
+// buildings in tile - which will be somewhat tied to the tile as I want buildings to basically be moveless units.
+// Maybe buildings get a marker component or trait, then if you want something to be a building its the same
+// stuff as a unit but they get that marker component/trait and it holds them in a separate spot
+
+
+use bevy::prelude::{Component, Entity, reflect_trait, Resource};
+use downcast_rs::DowncastSync;
+
+/// Marker component on map tiles for ease of query and accessing
+#[derive(Component)]
+pub struct Tile;
+
+/// Marker component on map tiles for ease of query and accessing
+//#[derive(Resource)]
+//pub struct TerrainInfo(HashMap<dyn TerrainExtensionTraitBase, dyn TerrainBaseTraitBase>);
+
+/// Component holding the tile terrain info needed by any built in logic.
+/// Terrain type
+#[derive(Component)]
+pub struct TileTerrainInfo {
+    pub(crate) terrain_extension_type: Box<dyn TerrainExtensionTraitBase>
+    //pub terrain_extension_type: TerrainExtensionType
+
+}
+
+pub struct TerrainBaseType{
+    name: &'static str,
+}
+
+pub struct TerrainExtensionType{
+    name: &'static str,
+    texture_index: u32,
+    terrain_base_type: &'static TerrainBaseType,
+}
+
+
+pub const TERRAIN_BASE_TYPES: &'static [TerrainBaseType] = &[
+    TerrainBaseType{ name: "Ground" },
+    TerrainBaseType{ name: "Water" }
+
+];
+
+pub const TERRAIN_EXTENSION_TYPES: &'static [TerrainExtensionType] = &[
+    TerrainExtensionType{ name: "Grassland", texture_index: 0, terrain_base_type: &TERRAIN_BASE_TYPES[0] },
+    TerrainExtensionType{ name: "Hill", texture_index: 3, terrain_base_type: &TERRAIN_BASE_TYPES[0] },
+    TerrainExtensionType{ name: "Ocean", texture_index: 6, terrain_base_type: &TERRAIN_BASE_TYPES[1] },
+
+];
+
+
+pub trait TerrainBaseTraitBase: DowncastSync {}
+downcast_rs::impl_downcast!(sync TerrainBaseTraitBase);
+
+pub trait TerrainBaseTrait: TerrainBaseTraitBase {
+    /// A unique name to identify your Terrain Base Type, this needs to be unique __across all included crates__
+    ///
+    /// A good combination is crate name + struct name
+    const NAME: &'static str;
+}
+
+/// You will also have to mark it with either [`ServerMessage`] or [`ClientMessage`] (or both)
+/// to signal which direction this message can be sent.
+pub trait TerrainExtensionTraitBase: DowncastSync {
+    
+    fn return_name(&self) -> &str;
+    fn return_texture_id(&self) -> &u32;
+    fn return_terrain_base_trait(&self) -> &dyn TerrainBaseTraitBase;
+    
+}
+downcast_rs::impl_downcast!(sync TerrainExtensionTraitBase);
+
+pub trait TerrainExtensionTrait: TerrainExtensionTraitBase {
+    /// A unique name to identify your Terrain Extension Type, this needs to be unique __across all included crates__
+    ///
+    /// A good combination is crate name + struct name
+    const NAME: &'static str;
+    const TEXTURE_ID: &'static u32;
+    const TERRAIN_BASE: &'static dyn TerrainBaseTraitBase;
+    
+    fn return_name(&self) -> &str{
+        Self::NAME
+    }
+    fn return_texture_id(&self) -> &u32{
+        Self::TEXTURE_ID
+    }
+}
+
+pub struct GroundTerrainBase {}
+
+impl TerrainBaseTraitBase for GroundTerrainBase {}
+impl TerrainBaseTrait for GroundTerrainBase {
+    const NAME: &'static str = "Bevy_ggf_GroundTerrainBase";
+}
+
+pub struct WaterTerrainBase {}
+
+impl TerrainBaseTraitBase for WaterTerrainBase {}
+impl TerrainBaseTrait for WaterTerrainBase {
+    const NAME: &'static str = "Bevy_ggf_WaterTerrainBase";
+}
+
+pub struct Grassland {}
+
+impl TerrainExtensionTraitBase for Grassland {
+    fn return_name(&self) -> &str {
+        "Bevy_ggf_Grassland"    
+    }
+
+    fn return_texture_id(&self) -> &u32 {
+        &0
+    }
+
+    fn return_terrain_base_trait(&self) -> &dyn TerrainBaseTraitBase {
+        &GroundTerrainBase{}
+    }
+}
+
+
+pub struct Hill {}
+
+impl TerrainExtensionTraitBase for Hill {
+    fn return_name(&self) -> &str {
+        "Bevy_ggf_Hill"
+    }
+
+    fn return_texture_id(&self) -> &u32 {
+        &3
+    }
+
+    fn return_terrain_base_trait(&self) -> &dyn TerrainBaseTraitBase {
+        &GroundTerrainBase{}
+    }
+}
+
+
+pub struct Ocean {}
+
+impl TerrainExtensionTraitBase for Ocean {
+    fn return_name(&self) -> &str {
+        "Bevy_ggf_Ocean"
+    }
+
+    fn return_texture_id(&self) -> &u32 {
+        &6
+    }
+
+    fn return_terrain_base_trait(&self) -> &dyn TerrainBaseTraitBase {
+        &WaterTerrainBase{}
+    }
+}
+
