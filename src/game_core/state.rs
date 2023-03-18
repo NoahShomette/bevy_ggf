15ad1a1cff1f90c663fef838d8332ad7bae3a541
Noah <noahshomette@gmail.com>
2023-03-18 01:16:37 -0400

    work on state

---
+++ src/game_core/state.rs
@@ -0,0 +1,32 @@
+﻿use bevy::prelude::Reflect;
+use bevy_ecs_tilemap::tiles::TilePos;
+use crate::object::ObjectId;
+
+pub enum StateType{
+    Object{
+        object_id: ObjectId
+    },
+    Tile{
+        tile_pos: TilePos
+    },
+    Resource,
+    Player{
+        object_id: ObjectId
+    },
+}
+
+pub enum ChangeType {
+    Modified,
+    Spawned,
+    Despawned,
+}
+
+pub struct StateEvent {
+    state: Vec<StateChange>
+}
+
+pub struct StateChange{
+    state_type: StateType,
+    change_type: ChangeType,
+    updated_state: Vec<Box<dyn Reflect>>
+}
